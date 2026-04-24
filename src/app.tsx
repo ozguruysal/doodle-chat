f0bcff09c9d3269e3bf68f0f8f391c83cec4725e
Özgür Uysal <ozguruysal@gmail.com>
2026-04-24 16:41:47 +0300

    api layer implementation

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
+import { getMessages } from "./features/chat/api/messages";
+
 function App() {
+  useEffect(() => {
+    getMessages()
+      .then((messages) => {
+        console.log(messages);
+      })
+      .catch((error) => {
+        console.error("Error fetching messages:", error);
+      });
+  }, []);
+
   return <div>Hello Vite, React</div>;
 }
 
