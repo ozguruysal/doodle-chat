0c37077ac36fdd842f1bf8be06a2fa1997c13205
Özgür Uysal <ozguruysal@gmail.com>
2026-04-28 15:31:38 +0300

    Add description

--- src/features/chat/utils/decode-html.ts
+++ src/features/chat/utils/decode-html.ts
@@ -1,6 +1,9 @@
 const textarea =
   typeof window !== "undefined" ? document.createElement("textarea") : null;
 
+/**
+ * Decodes HTML entities in a string into their corresponding characters.
+ */
 export function decodeHtml(html: string): string {
   if (!textarea) {
     return html;
